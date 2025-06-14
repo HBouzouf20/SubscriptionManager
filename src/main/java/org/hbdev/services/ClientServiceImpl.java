79019aaa5087cdf36b765c344b2a06182f7b0a11
HBouzouf20 <bouzouf.hamza.dev@gmail.com>
2025-06-14 21:33:11 +0100

    clean service

--- src/main/java/org/hbdev/services/ClientServiceImpl.java
+++ src/main/java/org/hbdev/services/ClientServiceImpl.java
@@ -1,53 +1,65 @@
 package org.hbdev.services;
 
-import org.hbdev.daos.ClientDao;
+import lombok.extern.java.Log;
 import org.hbdev.daos.ClientDaoImpl;
 import org.hbdev.models.Client;
 
+import java.util.List;
+
+@Log
 public class ClientServiceImpl implements ClientService {
-    private final ClientDao clientDao = ClientDaoImpl.instance;
+
+    private final ClientDaoImpl clientDao = ClientDaoImpl.instance;
+
     @Override
-    public Client save(Client entity) {
-        return clientDao.save(entity);
+    public Client save(Client client) {
+        log.info("Saving client: " + client);
+        return clientDao.save(client);
     }
 
     @Override
-    public void deleteById(Integer integer) {
-
+    public Client update(Client client, Integer id) {
+        log.info("Updating client with ID " + id);
+        return clientDao.update(client, id);
     }
 
     @Override
-    public Client findById(Integer id) {
-        return clientDao.findById(id);
+    public void deleteById(Integer id) {
+        log.info("Deleting client by ID: " + id);
+        clientDao.deleteById(id);
     }
 
     @Override
-    public Iterable<Client> findAll() {
-        return clientDao.findAll();
-    }
-
-    @Override
-    public void delete(Client entity) {
-
+    public void delete(Client client) {
+        log.info("Deleting client: " + client);
+        clientDao.delete(client);
     }
 
     @Override
     public void deleteAll() {
-
+        log.warning("Deleting all clients.");
+        clientDao.deleteAll();
     }
 
     @Override
-    public Client update(Client entity, Integer integer) {
-        return clientDao.update(entity, integer);
+    public Client findById(Integer id) {
+        log.info("Finding client by ID: " + id);
+        return clientDao.findById(id);
+    }
+
+    @Override
+    public List<Client> findAll() {
+        log.info("Fetching all clients.");
+        return (List<Client>) clientDao.findAll();
     }
 
     @Override
     public long count() {
-        return 0;
+        return findAll().size();
     }
 
     @Override
-    public boolean existsById(Integer integer) {
-        return false;
+    public boolean existsById(Integer id) {
+        return findById(id)!=null;
     }
 }
